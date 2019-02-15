963db4ff11984236452d4df3493c38a5361c04ea
baesparza <bruno.be81@gmail.com>
2019-02-15 10:16:17 -0500

    style: add styled container

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UiComponentsModule } from './modules/ui-components/ui-components.module';
+import { ContainerComponent } from './components/container/container.component';
+import { ButtonComponent, AnchorComponent } from './components/button/button.component';
+
+const DECLARATIONS = [ContainerComponent, ButtonComponent, AnchorComponent];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    UiComponentsModule
-  ],
-  exports: [
-    CommonModule,
-    UiComponentsModule
-  ],
-
+  declarations: DECLARATIONS,
+  imports: [CommonModule],
+  exports: [CommonModule, ...DECLARATIONS]
 })
-export class SharedModule { }
+export class SharedModule {}
