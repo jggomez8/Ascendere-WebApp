a61108d2356afdfee56c3fb789f81bdb35108f2d
baesparza <bruno.be81@gmail.com>
2019-02-17 16:57:39 -0500

    style: add gloval card styles

--- src/app/shared/components/cards/curso-card/curso-card.component.ts
+++ src/app/shared/components/cards/curso-card/curso-card.component.ts
@@ -7,7 +7,13 @@ import { Component } from '@angular/core';
 })
 export class CursoCardComponent {
   title = 'Nombre Encuentro Prueba Esto es muy Largo';
-  type = 'investigacion';
+  type = 'investigacion'; // TODO: get type as background
   starDate = new Date();
   endDate = new Date();
+
+  get background(): object {
+    if (this.type === 'investigacion')
+      return { 'background-image': 'linear-gradient(to bottom right,#ffb31b,#ffaa00)' };
+    return { 'background-image': 'linear-gradient(to bottom right,#ffb31b,#ffaa00)' };
+  }
 }
