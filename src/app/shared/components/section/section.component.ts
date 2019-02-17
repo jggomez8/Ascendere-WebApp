5aca2187f28a147caf759abf2f95f24dde46f4bd
baesparza <bruno.be81@gmail.com>
2019-02-17 01:03:21 -0500

    feat(card): fix style  esrrors

--- src/app/shared/components/section/section.component.ts
+++ src/app/shared/components/section/section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'section[indev-section]',
@@ -29,8 +29,4 @@ import { Component, OnInit } from '@angular/core';
     `
   ]
 })
-export class SectionComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class SectionComponent {}
