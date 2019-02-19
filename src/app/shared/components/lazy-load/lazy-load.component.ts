5722d4b4d3b8e4070e568e838bdebd7f0d5dc49e
baesparza <bruno.be81@gmail.com>
2019-02-19 17:09:44 -0500

    feat(lazy load  image ): add fade transition

--- src/app/shared/components/lazy-load/lazy-load.component.ts
+++ src/app/shared/components/lazy-load/lazy-load.component.ts
@@ -8,12 +8,12 @@ const TRANSITIONS = ['fade-in', 'translate'];
   exportAs: 'LazyTransitionComponent',
   template: `
     <div class="lazy-load">
-      <ng-content *ngIf="loaded"></ng-content>
+      <ng-content></ng-content>
     </div>
   `,
   styleUrls: ['./lazy-load.component.scss']
 })
-export class LazyTransitionComponent implements OnInit,  AfterViewInit {
+export class LazyTransitionComponent implements OnInit, AfterViewInit {
   // Transition elements will use when loaded
   @Input() transition: string;
 
@@ -22,22 +22,23 @@ export class LazyTransitionComponent implements OnInit,  AfterViewInit {
   // state of current element.
   public loaded = false;
 
-  constructor(public _element: ElementRef) { }
+  constructor(public _element: ElementRef) {}
 
   ngOnInit(): void {
     // set transition if included or define to defaukt transition
-    this.transition = TRANSITIONS.includes(this.transition) ? this.transition : 'fade-in'
-      ;
-  }
-
-  ngAfterViewInit() {
+    this.transition = TRANSITIONS.includes(this.transition) ? this.transition : 'fade-in';
     this._intersectionObserver = new IntersectionObserver(this.checkForIntersection);
     this._intersectionObserver.observe(this._element.nativeElement as Element);
   }
 
+  ngAfterViewInit() {}
+
   private checkForIntersection = (entries: Array<IntersectionObserverEntry>) => {
     entries.forEach((entry: IntersectionObserverEntry) => {
-      if (this.checkIfIntersecting(entry)) {
+      if (entry.isIntersecting) {
+        console.log('touch');
+
+        // if (this.checkIfIntersecting(entry)) {
         this.loaded = true;
         this._intersectionObserver.unobserve(<Element>this._element.nativeElement);
         this._intersectionObserver.disconnect();
@@ -54,14 +55,21 @@ export class LazyTransitionComponent implements OnInit,  AfterViewInit {
   selector: 'indev-lazy-image',
   exportAs: 'LazyImageComponent',
   template: `
-      <img *ngIf="loaded" [src]="src" alt="TODO: temp img" />
+    <div class="lazy-container" [ngClass]="{ fade: loaded }">
+      <img *ngIf="loaded" [src]="dataSource" alt="TODO: temp img" />
+    </div>
   `,
   styleUrls: ['./lazy-load.component.scss']
 })
 export class LazyLoadImage extends LazyTransitionComponent {
-  @Input() src: string;
+  @Input() dataSource: string;
 
   constructor(public _element: ElementRef) {
     super(_element);
+  }
+
+  get src() {
+    if (this.loaded) return this.dataSource;
+    return '';
   }
 }
