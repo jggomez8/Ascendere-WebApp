bee2dfd407a3f66e6583107ec0c6eb27dd1f4d2f
baesparza <bruno.be81@gmail.com>
2019-03-04 23:31:41 -0500

    feat(encuentros): add vertical list

--- src/app/modules/cafe-cientifico/cafe-cientifico.module.ts
+++ src/app/modules/cafe-cientifico/cafe-cientifico.module.ts
@@ -5,9 +5,10 @@ import { CafeCientificoRoutingModule } from './cafe-cientifico.routing';
 import { CafeCientificoComponent } from './pages/cafe-cientifico/cafe-cientifico.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HeroEncuentroComponent } from './components/hero-encuentro/hero-encuentro.component';
+import { CarteleraEncuentrosComponent } from './components/cartelera-encuentros/cartelera-encuentros.component';
 
 @NgModule({
-  declarations: [CafeCientificoComponent, HeroEncuentroComponent],
+  declarations: [CafeCientificoComponent, HeroEncuentroComponent, CarteleraEncuentrosComponent],
   imports: [
     CommonModule,
     CafeCientificoRoutingModule,
