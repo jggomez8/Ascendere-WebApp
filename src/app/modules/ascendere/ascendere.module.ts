ff47652bcc5332c1501ff3f9492864f29dd6658c
baesparza <bruno.be81@gmail.com>
2019-03-05 20:48:11 -0500

    feat(ascendere): add and style cards

--- src/app/modules/ascendere/ascendere.module.ts
+++ src/app/modules/ascendere/ascendere.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AscendereRoutingModule } from './ascendere.routing';
 import { AscendereComponent } from './pages/ascendere/ascendere.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ServicioCardComponent } from './components/servicio-card/servicio-card.component';
 
 @NgModule({
-  declarations: [AscendereComponent],
+  declarations: [AscendereComponent, ServicioCardComponent],
   imports: [CommonModule, AscendereRoutingModule, SharedModule]
 })
 export class AscendereModule {}
--- src/assets/markdown/vitamina-i/vitamina-i.md
+++ src/assets/markdown/vitamina-i/vitamina-i.md
@@ -1,4 +1,4 @@
-Master Class Vitamina I, son espacios en el que varios docentes compartirán sus experiencias y conocimientos sobre metodologías docentes que están aplicando en el aprendizaje de nuestros estudiantes.
+**Master Class Vitamina I**, son espacios en el que varios docentes compartirán sus experiencias y conocimientos sobre metodologías docentes que están aplicando en el aprendizaje de nuestros estudiantes.
 
 
 - [Conoce Nuestra Oferta]()
