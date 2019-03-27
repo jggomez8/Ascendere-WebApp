56d4259042715678c7ee4d3ad20f8197e0b82c80
baesparza <bruno.be81@gmail.com>
2019-03-26 23:26:28 -0500

    feat(*): update

--- src/app/modules/noticias/pages/noticia-detail/noticia-detail.component.ts
+++ src/app/modules/noticias/pages/noticia-detail/noticia-detail.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Noticia } from 'src/app/interfaces/noticia';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'indev-noticia-detail',
   templateUrl: './noticia-detail.component.html',
   styleUrls: ['./noticia-detail.component.scss']
 })
-export class NoticiaDetailComponent implements OnInit {
+export class NoticiaDetailComponent implements OnInit, OnDestroy {
+  noticia: Noticia;
 
-  constructor() { }
+  private _noticiaSub: Subscription;
+
+  constructor(private _route: ActivatedRoute) {}
 
   ngOnInit() {
+    this._noticiaSub = this._route.data.subscribe(
+      data => {
+        this.noticia = data['noticia'] as Noticia;
+      },
+      err => console.error('TODO: do something')
+    );
   }
 
+  ngOnDestroy() {
+    this._noticiaSub.unsubscribe();
+  }
 }
