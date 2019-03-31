838851e13ea39a1cb3ba5a00123a95162720b3b6
baesparza <bruno.be81@gmail.com>
2019-03-31 13:17:11 -0500

    feat(desarrollo-asignatura): add style

--- src/app/modules/desarrollo-asignatura/desarrollo-asignatura.module.ts
+++ src/app/modules/desarrollo-asignatura/desarrollo-asignatura.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DesarrolloAsignaturaComponent } from './pages/desarrollo-asignatura/desarrollo-asignatura.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DesarrolloAsignaturaRoutingModule } from './desarrollo-asignatura.routing';
+import { FaqCardComponent } from './components/faq-card/faq-card.component';
 
 @NgModule({
-  declarations: [DesarrolloAsignaturaComponent],
+  declarations: [DesarrolloAsignaturaComponent, FaqCardComponent],
   imports: [CommonModule, SharedModule, DesarrolloAsignaturaRoutingModule]
 })
 export class DesarrolloAsignaturaModule {}
---
+++ src/assets/markdown/desarrollo-asignatura/asesoramiento-pedagogico.md
@@ -0,0 +1,8 @@
+#### ¿Necesitas asesoramiento pedagógico para el desarrollo de tu asignatura?
+
+Desde el Laboratorio de Innovación e Investigación Docente (LiiD) te ofrecemos varios servicios personalizados que te servirán para mejorar tu práctica docente:
+
+- Filmación de tu clase
+- Observación de tu clase
+- Asesoramiento personalizado
+- Grabación de Vídeos
